2376e8946586ec4470dbe60d28ba80d35518b23a
nitinshukla <nitin.shukla12004@gmail.com>
2025-10-16 19:22:52 +0530

    added signUp route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,44 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '../src/generated/prisma/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{ // In Hono, we can define types for various things like env, bindings, etc.
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>()
 
 // Here c in the callback function is the context object
 // It contains request and response objects among other things
-app.get('/', (c) => {
+// app.get('/', (c) => {
 
-  // This Prisma Client is not declared globally because sometimes in serverless environments we lose the global state so try to avoid declaring variable globally
-  const prisma = new PrismaClient({
-    datasourceUrl : env.DATABASE_URL,
-  }).$extends(withAccelerate())
-  return c.text('Hello Hono!')
-})
+//   // This Prisma Client is not declared globally because sometimes in serverless environments we lose the global state so try to avoid declaring variable globally. Also, In Serverless environments, we can't access the env variables outside the handler function as each handler is independently deployed.
+
+//   const prisma = new PrismaClient({
+//     datasourceUrl : c.env.DATABASE_URL,
+//   }).$extends(withAccelerate())
+//   return c.text('Hello Hono!')
+// })
 
 // Ideally we should have a separate route file for each resource but it's fine for now
 
 // For sign up
 app.post('/api/v1/signUp', async(c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json()
+  const user = await prisma.user.create({
+    data : {
+      email : body.email,
+      password : body.password
+    }
+  })
+  const token = await sign({ id : user.id }, c.env.JWT_SECRET)
 
+  return c.json({ jwt : token })
 })
 // For sign in
 app.post('/api/v1/signIn', async(c) => {
